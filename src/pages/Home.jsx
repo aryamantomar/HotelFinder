77665ad543ddd32da79fd4f8f0b83a7b88e4c6ad
Aryaman-Tomar <aryamantomar2004@gmail.com>
2026-07-05 18:24:34 +0530

    updated

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h1>🏨 HotelFinder</h1>
+            <h2>Home Page</h2>
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/pages/HotelDetails.jsx
@@ -0,0 +1,9 @@
+function HotelDetails() {
+    return (
+        <div>
+            <h1>Hotel Details</h1>
+        </div>
+    );
+}
+
+export default HotelDetails;
