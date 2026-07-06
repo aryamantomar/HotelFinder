3d6b923cc6157858ab613c516d77185e9df55c28
Aryaman-Tomar <aryamantomar2004@gmail.com>
2026-07-06 13:44:20 +0530

    update 1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import Hotels from "./pages/Hotels";
 import HotelDetails from "./pages/HotelDetails";
+import Hotels from "./pages/Hotels";
 import Booking from "./pages/Booking";
 import BookingSuccess from "./pages/BookingSuccess";
 import NotFound from "./pages/NotFound";
 
 function App() {
     return (
-        <BrowserRouter basename="/HotelFinder">
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
             <Routes>
-
                 <Route path="/" element={<Home />} />
-
                 <Route path="/hotels" element={<Hotels />} />
-
                 <Route path="/hotel/:id" element={<HotelDetails />} />
-
                 <Route path="/booking/:id" element={<Booking />} />
-
-                <Route
-                    path="/booking-success"
-                    element={<BookingSuccess />}
-                />
-
+                <Route path="/booking-success" element={<BookingSuccess />} />
                 <Route path="*" element={<NotFound />} />
-
             </Routes>
         </BrowserRouter>
     );
